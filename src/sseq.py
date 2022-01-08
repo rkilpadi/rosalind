9675e0be7f0056808d6128be1b2d127b6454ab63
rkilpadi <ryan@kilpadi.com>
2022-01-08 02:57:58 -0500

    Transitions and Transversions

--- src/sseq.py
+++ src/sseq.py
@@ -3,13 +3,12 @@ from Bio import SeqIO
 
 # Given two strings, returns the indices of a subsequence of the second string within the first
 # A subsequence is a collection of symbols contained in order (though not necessarily contiguously)
-# NOTE: counts from 1, not 0
 def find_subsequence(string, sub):
     seq_idx, string_idx = 0, 0
     indices = []
     for char in string:
         if char == sub[seq_idx]:
-            indices.append(string_idx+1)
+            indices.append(string_idx)
             seq_idx += 1
             if seq_idx >= len(sub):
                 break
@@ -21,5 +20,4 @@ if __name__ == '__main__':
     records = list(SeqIO.parse('data/rosalind_sseq.txt', 'fasta'))
     vals = find_subsequence(records[0].seq, records[1].seq)
     for val in vals:
-        print(val)
-
+        print(val+1)
---
+++ src/tran.py
@@ -0,0 +1,22 @@
+from Bio import SeqIO
+
+
+#
+def transition_transversion_ratio(seq1, seq2):
+    transitions, transversions = 0, 0
+    transitions_set1 = ['A', 'G']
+    transitions_set2 = ['C', 'T']
+    for nuc1, nuc2 in zip(seq1, seq2):
+        if nuc1 != nuc2:
+            if nuc1 in transitions_set1 and nuc2 in transitions_set1:
+                transitions += 1
+            elif nuc1 in transitions_set2 and nuc2 in transitions_set2:
+                transitions += 1
+            else:
+                transversions += 1
+    return transitions/transversions
+
+
+if __name__ == "__main__":
+    records = list(SeqIO.parse('data/rosalind_tran.txt', 'fasta'))
+    print(transition_transversion_ratio(records[0].seq, records[1].seq))
