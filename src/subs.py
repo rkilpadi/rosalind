9ef46bef92730ff995329e8d58789f4ed10a6724
rkilpadi <ryan@kilpadi.com>
2022-01-07 20:43:56 -0500

    Finding a Motif in DNA

---
+++ src/subs.py
@@ -0,0 +1,16 @@
+# Returns all locations of substring within a string
+def find_motifs(filename):
+    with open(filename, 'r') as data:
+        lines = data.read().splitlines()
+    string = lines[0]
+    substring = lines[1]
+
+    i, j = 0, len(substring)
+    while j <= len(string):
+        if string[i:j] == substring:
+            print(i+1)
+        i += 1
+        j += 1
+
+
+find_motifs('data/rosalind_subs.txt')
