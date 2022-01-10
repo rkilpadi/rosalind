9286c68af5e0453ea3df260b1598c040e045eb78
rkilpadi <ryan@kilpadi.com>
2022-01-10 01:52:38 -0500

    Locating Restriction Sites (fixed)

--- src/revp.py
+++ src/revp.py
@@ -9,22 +9,22 @@ def reverse_palindromes(dna, min_len, max_len):
     complement = revc.dna_complement(dna)  # used to verify substrings
     current = dna[:max_len]  # substring being tested
     i = 0
-    while i < len(dna) - min_len:
+    while i <= len(dna) - min_len:
         # shrink current by 2 until min_len is reached, then increment index and repeat
         if len(current) >= min_len:
             if current == complement[len(dna) - i - len(current):len(dna) - i]:
-                restriction_sites.append((i+1, len(current)))  # counts from 1
+                restriction_sites.append((i+1, len(current), current))  # counts from 1
             current = dna[i:i+len(current)-2]
         else:
             i += 1
             current = dna[i:i + max_len]
-            # ensure substrings are of even length (must be even to pass)
+            # ensure substrings are of even length (required for reverse palindromes)
             if len(current) % 2 != 0:
                 current = current[:-1]
     return restriction_sites
 
 
 if __name__ == "__main__":
-    seq = list(SeqIO.parse('../data/test.txt', 'fasta'))[0].seq
+    seq = list(SeqIO.parse('../data/rosalind_revp.txt', 'fasta'))[0].seq
     for restriction_site in reverse_palindromes(str(seq), 4, 12):
         print(str(restriction_site[0]) + ' ' + str(restriction_site[1]))
